82ec874f6765fc232d93124c83e80c643a2bccf3
MariaChumerina <masha.chumerina@gmail.com>
2021-03-16 19:26:20 +0500

    change value of access-control-allow-origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     next();
 });
 
