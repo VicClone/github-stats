3d15d7f05dddc0df17d8267b8ca0ffa4fe53594d
Victor <evdokimovvik@gmail.com>
2021-03-24 08:28:51 +0500

    add material ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,8 +5,35 @@ import { getOauthAuthorizeLink } from '../utils/Oauth';
 import { loginUserAction } from '../store/actions';
 import { AuthContextType } from '../types/appTypes';
 import { authenticate } from '../models/api';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        wrapper: {
+            margin: theme.spacing(1),
+            position: 'relative'
+        },
+        buttonProgress: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginTop: -12,
+            marginLeft: -12
+        }
+    })
+);
 
 export const Login: React.FC = () => {
+    const classes = useStyles();
+
     const {
         state: { clientId, redirectUri, proxyUrl, isLoggedIn },
         dispatch
@@ -37,29 +64,57 @@ export const Login: React.FC = () => {
     }
 
     return (
-        <section className="container">
-            <h1>Добро пожаловать</h1>
-            <p className="content-container">
-                Здесь можно посмотреть статистику гитхаба интересующего вас пользователя
-            </p>
-            {data.errorMessage && <span>{data.errorMessage}</span>}
-            <div className="login-container">
-                {data.isLoading ? (
-                    <div className="loader-container">
-                        <div className="loader">loading</div>
-                    </div>
-                ) : (
-                    <a
-                        className="login-link"
-                        href={getOauthAuthorizeLink(clientId, redirectUri)}
-                        onClick={() => {
-                            setData({ ...data, errorMessage: '' });
-                        }}
-                    >
-                        <span>Войти с помощью GitHub</span>
-                    </a>
-                )}
-            </div>
-        </section>
+        <Container maxWidth="sm">
+            <Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h3" component="h2">
+                            Добро пожаловать
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Здесь можно посмотреть статистику гитхаба интересующего вас пользователя
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <div className={classes.wrapper}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                href={getOauthAuthorizeLink(clientId, redirectUri)}
+                                disabled={data.isLoading}
+                            >
+                                Войти с помощью GitHub
+                            </Button>
+                            <CircularProgress size={24} />
+                            {data.isLoading && <CircularProgress size={24} />}
+                        </div>
+                    </CardActions>
+                </CardActionArea>
+            </Card>
+            <section className="container">
+                <h1>Добро пожаловать</h1>
+                <p className="content-container">
+                    Здесь можно посмотреть статистику гитхаба интересующего вас пользователя
+                </p>
+                {data.errorMessage && <span>{data.errorMessage}</span>}
+                <div className="login-container">
+                    {data.isLoading ? (
+                        <div className="loader-container">
+                            <div className="loader">loading</div>
+                        </div>
+                    ) : (
+                        <a
+                            className="login-link"
+                            href={getOauthAuthorizeLink(clientId, redirectUri)}
+                            onClick={() => {
+                                setData({ ...data, errorMessage: '' });
+                            }}
+                        >
+                            <span>Войти с помощью GitHub</span>
+                        </a>
+                    )}
+                </div>
+            </section>
+        </Container>
     );
 };
