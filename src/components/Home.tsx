dac307cb21e610150be02b302a735e77f752a4fa
Victor <evdokimovvik@gmail.com>
2021-03-27 10:45:20 +0500

    user card

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,6 +25,14 @@ import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import EmailIcon from '@material-ui/icons/Email';
+import WorkIcon from '@material-ui/icons/Work';
+import LanguageIcon from '@material-ui/icons/Language';
+import StarIcon from '@material-ui/icons/Star';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -165,22 +173,46 @@ export const Home: React.FC = () => {
                 <Grid item xs={6}>
                     <Card>
                         <CardHeader
-                            avatar={<Avatar aria-label="recipe">R</Avatar>}
+                            avatar={
+                                <Avatar
+                                    alt="name name"
+                                    src="https://material-ui.com/static/images/avatar/1.jpg"
+                                ></Avatar>
+                            }
                             action={
                                 <IconButton aria-label="settings">
                                     <MoreVertIcon />
                                 </IconButton>
                             }
-                            title="Shrimp and Chorizo Paella"
-                            subheader="September 14, 2016"
+                            title="Name"
+                            subheader="Локация"
                         />
                         <CardContent>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-                            </Typography>
+                            <List>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <EmailIcon></EmailIcon>
+                                    </ListItemIcon>
+                                    <ListItemText>E-mail</ListItemText>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <WorkIcon></WorkIcon>
+                                    </ListItemIcon>
+                                    <ListItemText>Организация</ListItemText>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <LanguageIcon></LanguageIcon>
+                                    </ListItemIcon>
+                                    <ListItemText>Сайт</ListItemText>
+                                </ListItem>
+                            </List>
                         </CardContent>
                         <CardActions disableSpacing>
+                            <Typography component="div" variant="body1" color="textPrimary">
+                                Репозитории:
+                            </Typography>
                             <IconButton
                                 className={clsx(classes.expand, {
                                     [classes.expandOpen]: expanded
@@ -194,29 +226,24 @@ export const Home: React.FC = () => {
                         </CardActions>
                         <Collapse in={expanded} timeout="auto" unmountOnExit>
                             <CardContent>
-                                <Typography paragraph>Method:</Typography>
-                                <Typography paragraph>
-                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                                    minutes.
-                                </Typography>
-                                <Typography paragraph>
-                                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                                    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                                    chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                                    salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                                    minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                                </Typography>
-                                <Typography paragraph>
-                                    Add rice and stir very gently to distribute. Top with artichokes and peppers, and
-                                    cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes.
-                                    Reduce heat to medium-low, add reserved shrimp and mussels, tucking them down into
-                                    the rice, and cook again without stirring, until mussels have opened and rice is
-                                    just tender, 5 to 7 minutes more. (Discard any mussels that don’t open.)
-                                </Typography>
-                                <Typography>
-                                    Set aside off of the heat to let rest for 10 minutes, and then serve.
-                                </Typography>
+                                <List>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary="Репозиторий 1"
+                                            secondary={
+                                                <React.Fragment>
+                                                    <Typography component="div" variant="body2" color="textPrimary">
+                                                        <StarIcon /> 20
+                                                    </Typography>
+                                                    <Typography component="div" variant="body2" color="textPrimary">
+                                                        Язык
+                                                    </Typography>
+                                                </React.Fragment>
+                                            }
+                                        />
+                                    </ListItem>
+                                    <ListItem>репозиторий 2</ListItem>
+                                </List>
                             </CardContent>
                         </Collapse>
                     </Card>
