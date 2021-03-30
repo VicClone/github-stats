d1ada94c760c4fe3055fddab7534ee0bd6595c43
MariaChumerina <masha.chumerina@gmail.com>
2021-03-30 17:14:33 +0500

    map user and repo info

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { AuthContext } from '../App';
 import { logOutUserAction } from '../store/actions';
@@ -32,6 +32,7 @@ import LanguageIcon from '@material-ui/icons/Language';
 import StarIcon from '@material-ui/icons/Star';
 import SearchBar from 'material-ui-search-bar';
 import { Link } from '@material-ui/core';
+import { RepoData, UserData } from '../types/apiTypes';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -56,11 +57,54 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
+const getRepository = (userName: string, repoName: string) => {
+    getRepoInfo(userName, repoName)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    getRepoPullsList(userName, repoName)
+        .then(data => {
+            console.log('pulls:');
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    getRepoIssuesList(userName, repoName)
+        .then(data => {
+            console.log('issues:');
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+const getCommits = (userName: string, email: string) => {
+    getCommitsByUser(userName, email)
+        .then(data => {
+            console.log('issues:');
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
 export const Home: React.FC = () => {
     const {
         state: { isLoggedIn },
         dispatch
     } = useContext<AuthContextType>(AuthContext);
+
+    const [searchUserValue, setSearchUserValue] = useState<string>('');
+    const [userInfo, setUserInfo] = useState<UserData | null>(null);
+    const [userRepos, setUserRepos] = useState<RepoData[]>();
 
     if (!isLoggedIn) {
         return <Redirect to="/login" />;
@@ -74,6 +118,7 @@ export const Home: React.FC = () => {
         getUserData(userName)
             .then(data => {
                 console.log(data);
+                setUserInfo(data as UserData);
             })
             .catch(error => {
                 console.log(error);
@@ -82,128 +127,114 @@ export const Home: React.FC = () => {
         getUserRepos(userName)
             .then(data => {
                 console.log(data);
+                setUserRepos(data as RepoData[]);
             })
             .catch(error => {
                 console.log(error);
             });
     };
 
-    const getRepository = (userName: string, repoName: string) => {
-        getRepoInfo(userName, repoName)
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-
-        getRepoPullsList(userName, repoName)
-            .then(data => {
-                console.log('pulls:');
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-
-        getRepoIssuesList(userName, repoName)
-            .then(data => {
-                console.log('issues:');
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    const handleSearch = (searchValue: string) => {
+        setSearchUserValue(searchValue);
     };
 
-    const getCommits = (userName: string, email: string) => {
-        getCommitsByUser(userName, email)
-            .then(data => {
-                console.log('issues:');
-                console.log(data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    const searchUser = () => {
+        getUserInfo(searchUserValue);
     };
 
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
+    const renderUserInfo = () => {
+        return (
+            <CardContent>
+                <List>
+                    {userInfo?.email && (
+                        <ListItem>
+                            <ListItemIcon>
+                                <EmailIcon></EmailIcon>
+                            </ListItemIcon>
+                            <ListItemText>{userInfo?.email}</ListItemText>
+                        </ListItem>
+                    )}
+                    {userInfo?.company && (
+                        <ListItem>
+                            <ListItemIcon>
+                                <WorkIcon></WorkIcon>
+                            </ListItemIcon>
+                            <ListItemText>{userInfo?.company}</ListItemText>
+                        </ListItem>
+                    )}
+                    {userInfo?.blog && (
+                        <ListItem>
+                            <ListItemIcon>
+                                <LanguageIcon></LanguageIcon>
+                            </ListItemIcon>
+                            <ListItemText>Сайт</ListItemText>
+                        </ListItem>
+                    )}
+                </List>
+            </CardContent>
+        );
+    };
+
+    const renderReposInfo = () => {
+        console.log(userRepos);
+        return (
+            <CardContent>
+                <Typography component="span" variant="body1" color="textPrimary">
+                    Репозитории:
+                </Typography>
+                <List>
+                    {userRepos?.map(repo => {
+                        return (
+                            <ListItem key={repo.updatedAt}>
+                                <ListItemText
+                                    primary={repo.name}
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography component="span" variant="body2" color="textPrimary">
+                                                <StarIcon /> {repo.stargazersCount}
+                                            </Typography>
+                                            <Typography component="span" variant="body2" color="textPrimary">
+                                                {repo.language}
+                                            </Typography>
+                                        </React.Fragment>
+                                    }
+                                />
+                                <Link href="/repository/1">Link</Link>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </CardContent>
+        );
+    };
 
     return (
         <Container maxWidth="md">
             <Box mt={20}>
-                <SearchBar />
+                <SearchBar value={searchUserValue} onChange={handleSearch} onRequestSearch={searchUser} />
             </Box>
-            <Box mt={10}>
-                <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
-                    <Grid item xs={12}>
-                        <Card>
-                            <CardHeader
-                                avatar={
-                                    <Avatar
-                                        alt="name name"
-                                        src="https://material-ui.com/static/images/avatar/1.jpg"
-                                    ></Avatar>
-                                }
-                                action={
-                                    <IconButton aria-label="settings">
-                                        <MoreVertIcon />
-                                    </IconButton>
-                                }
-                                title="Name"
-                                subheader="Локация"
-                            />
-                            <CardContent>
-                                <List>
-                                    <ListItem>
-                                        <ListItemIcon>
-                                            <EmailIcon></EmailIcon>
-                                        </ListItemIcon>
-                                        <ListItemText>E-mail</ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemIcon>
-                                            <WorkIcon></WorkIcon>
-                                        </ListItemIcon>
-                                        <ListItemText>Организация</ListItemText>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemIcon>
-                                            <LanguageIcon></LanguageIcon>
-                                        </ListItemIcon>
-                                        <ListItemText>Сайт</ListItemText>
-                                    </ListItem>
-                                </List>
-                            </CardContent>
-                            <CardContent>
-                                <Typography component="span" variant="body1" color="textPrimary">
-                                    Репозитории:
-                                </Typography>
-                                <List>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary="Репозиторий 1"
-                                            secondary={
-                                                <React.Fragment>
-                                                    <Typography component="span" variant="body2" color="textPrimary">
-                                                        <StarIcon /> 20
-                                                    </Typography>
-                                                    <Typography component="span" variant="body2" color="textPrimary">
-                                                        Язык
-                                                    </Typography>
-                                                </React.Fragment>
-                                            }
-                                        />
-                                        <Link href="/repository/1">Link</Link>
-                                    </ListItem>
-                                    <ListItem>репозиторий 2</ListItem>
-                                </List>
-                            </CardContent>
-                        </Card>
+            {userInfo && (
+                <Box mt={10}>
+                    <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+                        <Grid item xs={12}>
+                            <Card>
+                                <CardHeader
+                                    avatar={<Avatar alt="name name" src={userInfo?.avatarUrl}></Avatar>}
+                                    action={
+                                        <IconButton aria-label="settings">
+                                            <MoreVertIcon />
+                                        </IconButton>
+                                    }
+                                    title={userInfo?.name}
+                                    subheader={userInfo?.location}
+                                />
+                                {renderUserInfo()}
+                                {renderReposInfo()}
+                            </Card>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            )}
         </Container>
     );
 };
